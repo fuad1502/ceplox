10b0956e870b51e1ae398e3e4adda64266a330b4
Fuad Ismail <fuad1502@gmail.com>
2023-12-19 21:04:57 +0700

    Moved error static method in Ceplox to public scope

--- ceplox/Ceplox.hpp
+++ ceplox/Ceplox.hpp
@@ -6,9 +6,9 @@ public:
   void static runFile(char *file_name);
   void static runPrompt();
   void static run(std::string source);
+  void static error(int line, std::string message);
 
 private:
   static bool hadError;
-  void static error(int line, std::string message);
   void static report(int line, std::string where, std::string message);
 };
