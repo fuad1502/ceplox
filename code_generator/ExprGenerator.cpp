cf196f987ee11769890a9bdd6c60cb809f817745
Fuad Ismail <fuad1502@gmail.com>
2023-12-20 18:05:43 +0700

    Create ExprVisitor generator; Restructured generated files into three files, Expr.hpp ExprVisitor.hpp and Expr.cpp; Modified CMakeLists to correctly handle generating source file instead of just header file

--- code_generator/ExprGenerator.cpp
+++ code_generator/ExprGenerator.cpp
@@ -14,6 +14,8 @@ private:
   static void generateClass(std::string className,
                             std::vector<std::string> fields,
                             std::ofstream &outputFile);
+  static void generateAbstractVisitor(std::ofstream &outputFile,
+                                      std::vector<std::string> grammar);
   static std::vector<std::string> parseGrammar(std::string grammarFilePath);
   static std::vector<std::string> parseFields(std::string fields);
   static std::string name(std::string field);
@@ -27,29 +29,68 @@ void ExprGenerator::generate(std::string grammarFilePath,
 
 void ExprGenerator::generate(std::vector<std::string> grammar,
                              std::string outputFilePath) {
-  std::ofstream outputFile(outputFilePath);
-  outputFile << "#include \"Token.hpp\"" << std::endl;
-  outputFile << "#include <memory>" << std::endl;
-  outputFile << std::endl;
-  outputFile << "class Expr {" << std::endl;
-  outputFile << "public:" << std::endl;
+  // Generate Expr header file
+  std::ofstream headerFile(outputFilePath + "/Expr.hpp");
+  // Guard
+  headerFile << "#pragma once" << std::endl;
+  // Header
+  headerFile << "#include \"Token.hpp\"" << std::endl;
+  headerFile << "#include <memory>" << std::endl;
+  headerFile << "#include <any>" << std::endl;
+  headerFile << std::endl;
+  // Forward declaration
+  headerFile << "class ExprVisitor;" << std::endl;
+  headerFile << std::endl;
+  // Base Expr class
+  headerFile << "class Expr {" << std::endl;
+  headerFile << "public:" << std::endl;
+  headerFile << "  virtual std::any accept(ExprVisitor &visitor) = 0;"
+             << std::endl;
+  headerFile << "};" << std::endl;
+  headerFile << std::endl;
+  // Subclasses
   for (auto rule : grammar) {
     auto className = rule.substr(0, rule.find(":"));
     boost::trim(className);
     auto fields = rule.substr(rule.find(":") + 1);
     boost::trim(fields);
-    generateClass(className, parseFields(fields), outputFile);
+    generateClass(className, parseFields(fields), headerFile);
+    headerFile << std::endl;
   }
-  outputFile << "};" << std::endl;
+
+  // Generate ExprVisitor interface
+  std::ofstream visitorHeaderFile(outputFilePath + "/ExprVisitor.hpp");
+  // Guard
+  visitorHeaderFile << "#pragma once" << std::endl;
+  // Header
+  visitorHeaderFile << "#include \"Expr.hpp\"" << std::endl;
+  visitorHeaderFile << std::endl;
+  generateAbstractVisitor(visitorHeaderFile, grammar);
+
+  // Generate Expr source file
+  std::ofstream sourceFile(outputFilePath + "/Expr.cpp");
+  sourceFile << "#include \"Expr.hpp\"" << std::endl;
+  sourceFile << "#include \"ExprVisitor.hpp\"" << std::endl;
+  sourceFile << std::endl;
+  for (auto rule : grammar) {
+    auto className = rule.substr(0, rule.find(":"));
+    boost::trim(className);
+    sourceFile << "std::any " << name(className)
+               << "Expr::accept(ExprVisitor &visitor) {" << std::endl;
+    sourceFile << "  return visitor.visit" << className << "(*this);"
+               << std::endl;
+    sourceFile << "}" << std::endl;
+    sourceFile << std::endl;
+  }
 }
 
 void ExprGenerator::generateClass(std::string className,
                                   std::vector<std::string> fields,
                                   std::ofstream &outputFile) {
-  outputFile << "  class " << className << " {" << std::endl;
-  outputFile << "  public:" << std::endl;
+  outputFile << "class " << className << "Expr : public Expr {" << std::endl;
+  outputFile << "public:" << std::endl;
   // Declare constructor
-  outputFile << "    " << className << "(";
+  outputFile << "  " << className << "Expr(";
   // Constructor parameters
   for (auto field : fields) {
     outputFile << field << ", ";
@@ -57,7 +98,7 @@ void ExprGenerator::generateClass(std::string className,
   outputFile.seekp(-2, std::ios_base::end);
   outputFile << ")" << std::endl;
   // Constructor initializer list
-  outputFile << "        : ";
+  outputFile << "      : ";
   for (auto field : fields) {
     outputFile << name(field) << "(std::move(" << name(field) << ")), ";
   }
@@ -65,9 +106,25 @@ void ExprGenerator::generateClass(std::string className,
   outputFile << "{};" << std::endl;
   // Class members
   for (auto field : fields) {
-    outputFile << "    " << field << ";" << std::endl;
+    outputFile << "  " << field << ";" << std::endl;
   }
-  outputFile << "  };" << std::endl;
+  // Visitor accept method
+  outputFile << "  std::any accept(ExprVisitor &visitor) override;"
+             << std::endl;
+  outputFile << "};" << std::endl;
+}
+
+void ExprGenerator::generateAbstractVisitor(std::ofstream &outputFile,
+                                            std::vector<std::string> grammar) {
+  outputFile << "class ExprVisitor {" << std::endl;
+  outputFile << "public:" << std::endl;
+  for (auto rule : grammar) {
+    auto className = rule.substr(0, rule.find(":"));
+    boost::trim(className);
+    outputFile << "  virtual std::any visit" << className << "(const "
+               << name(className) << "Expr &expression) = 0;" << std::endl;
+  }
+  outputFile << "};" << std::endl;
 }
 
 std::vector<std::string>
