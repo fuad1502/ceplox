58a801c8b5a9897ecdae158acdc07fa26dda6f81
Fuad Ismail <fuad1502@gmail.com>
2023-12-21 21:51:27 +0700

    Add getter method for Token class; Changed std::optional literal output stream function to templated function; Seperate out LiteralValue variant output stream function; Add the output stream function declarations to header file

--- ceplox/Token.cpp
+++ ceplox/Token.cpp
@@ -1,32 +1,37 @@
 #include "Token.hpp"
 #include <variant>
 
-std::ostream &operator<<(std::ostream &stream,
-                         const std::optional<LiteralValue> &literal);
-
 Token::Token(std::string lexeme, TokenType tokenType,
              std::optional<LiteralValue> literal, int line)
     : lexeme{lexeme}, tokenType{tokenType}, literal{literal}, line{line} {}
+
+std::string Token::getLexeme() const { return lexeme; }
+TokenType Token::getTokenType() const { return tokenType; }
+std::optional<LiteralValue> Token::getLiteral() const { return literal; }
+int Token::getLine() const { return line; }
 
 std::ostream &operator<<(std::ostream &stream, const Token &token) {
   return stream << token.tokenType << " " << token.lexeme << " "
                 << token.literal;
 }
 
-std::ostream &operator<<(std::ostream &stream,
-                         const std::optional<LiteralValue> &literal) {
-  if (literal.has_value()) {
-    auto variant = literal.value();
-    if (std::holds_alternative<bool>(variant)) {
-      return stream << std::get<bool>(variant);
-    } else if (std::holds_alternative<double>(variant)) {
-      return stream << std::get<double>(variant);
-    } else if (std::holds_alternative<std::string>(variant)) {
-      return stream << std::get<std::string>(variant);
-    } else {
-      return stream;
-    }
+template <typename T>
+std::ostream &operator<<(std::ostream &stream, const std::optional<T> &opt) {
+  if (opt.has_value()) {
+    return stream << opt.value();
   } else {
     return stream << "nullopt";
+  }
+}
+
+std::ostream &operator<<(std::ostream &stream, const LiteralValue &literal) {
+  if (std::holds_alternative<bool>(literal)) {
+    return stream << std::get<bool>(literal);
+  } else if (std::holds_alternative<double>(literal)) {
+    return stream << std::get<double>(literal);
+  } else if (std::holds_alternative<std::string>(literal)) {
+    return stream << std::get<std::string>(literal);
+  } else {
+    return stream;
   }
 }
--- ceplox/Token.hpp
+++ ceplox/Token.hpp
@@ -11,6 +11,12 @@ class Token {
 public:
   Token(std::string lexeme, TokenType tokenType,
         std::optional<LiteralValue> literal, int line);
+
+  std::string getLexeme() const;
+  TokenType getTokenType() const;
+  std::optional<LiteralValue> getLiteral() const;
+  int getLine() const;
+
   friend std::ostream &operator<<(std::ostream &stream, const Token &token);
 
 private:
@@ -19,3 +25,7 @@ private:
   std::optional<LiteralValue> literal;
   int line;
 };
+
+template <typename T>
+std::ostream &operator<<(std::ostream &stream, const std::optional<T> &opt);
+std::ostream &operator<<(std::ostream &stream, const LiteralValue &literal);
